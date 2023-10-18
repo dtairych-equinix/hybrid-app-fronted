da91c771a0c9eaf3eac4b1d344f81494209f5043
dtairych-equinix <dtairych@ap.equinix.com>
2023-10-18 14:32:43 +0200

    Optimised app

--- src/App.js
+++ src/App.js
@@ -70,6 +70,158 @@
 //   }));
 
 //   const costForLastRequestDisplay = responseTimes.length > 0 ? cumulativeCost - chartData[chartData.length - 2]?.cumulativeCost : 0;
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import {
+//   LineChart,
+//   Line,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+// } from 'recharts';
+// import './App.css'; // Import the CSS file
+
+// function App() {
+//   const [responseTimes, setResponseTimes] = useState([]);
+//   const [totalRecords, setTotalRecords] = useState(0);
+//   const [cumulativeDataSize, setCumulativeDataSize] = useState(0);
+//   const [selectedCostKey, setSelectedCostKey] = useState('Internet');
+//   const [selectedCostValue, setSelectedCostValue] = useState(1.0);
+//   const [cumulativeCost, setCumulativeCost] = useState(0);
+//   const [chartData, setChartData] = useState([]);
+
+//   useEffect(() => {
+//     const pollServer = async () => {
+//       try {
+//         // Fetch the current cost factor from the server with every poll
+//         const costFactorResponse = await axios.get('http://20.160.160.36:4000/get-cost-factor');
+//         const { selectedCostKey, selectedCostValue } = costFactorResponse.data;
+//         setSelectedCostKey(selectedCostKey);
+//         setSelectedCostValue(selectedCostValue);
+  
+//         const response = await axios.get(`http://20.160.160.36:4000/poll`);
+//         const data = response.data;
+  
+//         // Update the total records
+//         setTotalRecords(data.totalRecords);
+  
+//         // Add the new response time to the array
+//         setResponseTimes((prevResponseTimes) => [...prevResponseTimes, data.responseTime]);
+  
+//         // Limit the responseTimes array to the last 50 entries
+//         if (responseTimes.length > 50) {
+//           setResponseTimes((prevResponseTimes) => prevResponseTimes.slice(-50));
+//         }
+  
+//         // Calculate the cumulative data size in MB and update the state
+//         const dataSizeInMB = data.totalRecords * calculateDataSizePerRecordInMB();
+//         setCumulativeDataSize((prevCumulativeDataSize) => prevCumulativeDataSize + dataSizeInMB);
+  
+//         // Calculate the cost for the last request
+//         const costForLastRequest =
+//         data.totalRecords * calculateDataSizePerRecordInMB() * selectedCostValue;
+//         setCumulativeCost((prevCumulativeCost) => prevCumulativeCost + costForLastRequest);
+
+//         setChartData((prevChartData) => {
+//           const newEntry = {
+//               interval: prevChartData.length + 1,
+//               responseTime: data.responseTime,
+//               cumulativeCost: prevCumulativeCost + costForLastRequest
+//           };
+//           return [...prevChartData, newEntry].slice(-50);  // Keeping the last 50 data points
+//       });
+
+//       // Calculate costForLastRequestDisplay here
+//       const costForLastRequestDisplay =
+//         responseTimes.length > 0
+//           ? cumulativeCost - (responseTimes.length === 1 ? 0 : cumulativeCost - responseTimes[responseTimes.length - 2])
+//           : 0;
+      
+//       // Set costForLastRequestDisplay in state
+//       setCostForLastRequestDisplay(costForLastRequestDisplay);
+//       } catch (error) {
+//         console.log('Error fetching data -', error);
+//       }
+//     };
+  
+//     // Poll the server every 5 seconds and update the responseTimes state
+//     const interval = setInterval(pollServer, 5000);
+  
+//     // Cleanup the interval on component unmount
+//     return () => clearInterval(interval);
+//   }, [selectedCostValue, cumulativeCost]);
+
+//   const [costForLastRequestDisplay, setCostForLastRequestDisplay] = useState(0);
+  
+//   useEffect(() => {
+//     const latestResponseTimes = responseTimes.slice(-50);
+
+//     // Accumulate the cost from the state and distribute it to the data points
+//     let accumulatedCost = cumulativeCost - latestResponseTimes.reduce((acc, time) => acc + time * calculateDataSizePerRecordInMB() * selectedCostValue, 0);
+
+//     const newChartData = latestResponseTimes.map((time, idx) => {
+//         const costForThisRequest = time * calculateDataSizePerRecordInMB() * selectedCostValue;
+//         accumulatedCost += costForThisRequest;
+
+//         return {
+//             interval: idx + 1,
+//             responseTime: time,
+//             cumulativeCost: accumulatedCost
+//         };
+//     });
+
+//     setChartData(newChartData);
+// }, [responseTimes, cumulativeCost, selectedCostValue]);
+
+
+
+
+//   const calculateDataSizePerRecordInMB = () => {
+//     // Replace this with your actual data size calculation logic
+//     return 0.1; // Assuming each record is 0.1 MB for illustration purposes
+//   };
+
+//   // const costForLastRequestDisplay =
+//   // responseTimes.length > 0
+//   //   ? cumulativeCost - (responseTimes.length === 1 ? 0 : cumulativeCost - responseTimes[responseTimes.length - 2])
+//   //   : 0;
+      
+//   return (
+//     <div className="container">
+//       <h1>Database Response Time Over Time</h1>
+//       <LineChart width={600} height={300} data={chartData}>
+//         <CartesianGrid strokeDasharray="3 3" />
+//         <XAxis dataKey="interval" />
+//         <YAxis />
+//         <Tooltip />
+//         <Legend />
+//         <Line type="monotone" dataKey="responseTime" name="Response Time (s)" stroke="#8884d8" />
+//       </LineChart>
+//       <p>Total Records: {totalRecords}</p>
+//       <p>Last Response Time: {responseTimes.length > 0 ? responseTimes[responseTimes.length - 1] : 0} seconds</p>
+//       <h3>Cumulative Data Size Requested: {cumulativeDataSize.toFixed(2)} MB</h3>
+
+//       <p>Current Cost Factor Key: {selectedCostKey}</p>
+//       <p>Current Cost Factor Value: {selectedCostValue}</p>
+//       <p>Cost for Last Request: {costForLastRequestDisplay.toFixed(2)}</p>
+//       <LineChart width={600} height={300} data={chartData}>
+//         <CartesianGrid strokeDasharray="3 3" />
+//         <XAxis dataKey="interval" />
+//         <YAxis />
+//         <Tooltip />
+//         <Legend />
+//         <Line type="monotone" dataKey="cumulativeCost" name="Cumulative Cost" stroke="#82ca9d" />
+//       </LineChart>
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import {
@@ -81,7 +233,7 @@ import {
   Tooltip,
   Legend,
 } from 'recharts';
-import './App.css'; // Import the CSS file
+import './App.css';
 
 function App() {
   const [responseTimes, setResponseTimes] = useState([]);
@@ -91,11 +243,11 @@ function App() {
   const [selectedCostValue, setSelectedCostValue] = useState(1.0);
   const [cumulativeCost, setCumulativeCost] = useState(0);
   const [chartData, setChartData] = useState([]);
+  const [costForLastRequestDisplay, setCostForLastRequestDisplay] = useState(0);
 
   useEffect(() => {
     const pollServer = async () => {
       try {
-        // Fetch the current cost factor from the server with every poll
         const costFactorResponse = await axios.get('http://20.160.160.36:4000/get-cost-factor');
         const { selectedCostKey, selectedCostValue } = costFactorResponse.data;
         setSelectedCostKey(selectedCostKey);
@@ -104,81 +256,39 @@ function App() {
         const response = await axios.get(`http://20.160.160.36:4000/poll`);
         const data = response.data;
   
-        // Update the total records
         setTotalRecords(data.totalRecords);
   
-        // Add the new response time to the array
-        setResponseTimes((prevResponseTimes) => [...prevResponseTimes, data.responseTime]);
+        setResponseTimes((prevResponseTimes) => [...prevResponseTimes, data.responseTime].slice(-50));
   
-        // Limit the responseTimes array to the last 50 entries
-        if (responseTimes.length > 50) {
-          setResponseTimes((prevResponseTimes) => prevResponseTimes.slice(-50));
-        }
-  
-        // Calculate the cumulative data size in MB and update the state
         const dataSizeInMB = data.totalRecords * calculateDataSizePerRecordInMB();
         setCumulativeDataSize((prevCumulativeDataSize) => prevCumulativeDataSize + dataSizeInMB);
   
-        // Calculate the cost for the last request
-        const costForLastRequest =
-        data.totalRecords * calculateDataSizePerRecordInMB() * selectedCostValue;
+        const costForLastRequest = dataSizeInMB * selectedCostValue;
         setCumulativeCost((prevCumulativeCost) => prevCumulativeCost + costForLastRequest);
 
-      // Calculate costForLastRequestDisplay here
-      const costForLastRequestDisplay =
-        responseTimes.length > 0
-          ? cumulativeCost - (responseTimes.length === 1 ? 0 : cumulativeCost - responseTimes[responseTimes.length - 2])
-          : 0;
-      
-      // Set costForLastRequestDisplay in state
-      setCostForLastRequestDisplay(costForLastRequestDisplay);
+        setChartData((prevChartData) => {
+          const newEntry = {
+              interval: prevChartData.length + 1,
+              responseTime: data.responseTime,
+              cumulativeCost: prevCumulativeCost + costForLastRequest
+          };
+          return [...prevChartData, newEntry].slice(-50);
+        });
+
+        setCostForLastRequestDisplay(costForLastRequest);
       } catch (error) {
         console.log('Error fetching data -', error);
       }
     };
   
-    // Poll the server every 5 seconds and update the responseTimes state
     const interval = setInterval(pollServer, 5000);
-  
-    // Cleanup the interval on component unmount
     return () => clearInterval(interval);
-  }, [selectedCostValue, cumulativeCost]);
-
-  const [costForLastRequestDisplay, setCostForLastRequestDisplay] = useState(0);
-  
-  useEffect(() => {
-    const latestResponseTimes = responseTimes.slice(-50);
-
-    // Accumulate the cost from the state and distribute it to the data points
-    let accumulatedCost = cumulativeCost - latestResponseTimes.reduce((acc, time) => acc + time * calculateDataSizePerRecordInMB() * selectedCostValue, 0);
-
-    const newChartData = latestResponseTimes.map((time, idx) => {
-        const costForThisRequest = time * calculateDataSizePerRecordInMB() * selectedCostValue;
-        accumulatedCost += costForThisRequest;
-
-        return {
-            interval: idx + 1,
-            responseTime: time,
-            cumulativeCost: accumulatedCost
-        };
-    });
-
-    setChartData(newChartData);
-}, [responseTimes, cumulativeCost, selectedCostValue]);
-
-
-
+  }, []); 
 
   const calculateDataSizePerRecordInMB = () => {
-    // Replace this with your actual data size calculation logic
     return 0.1; // Assuming each record is 0.1 MB for illustration purposes
   };
 
-  // const costForLastRequestDisplay =
-  // responseTimes.length > 0
-  //   ? cumulativeCost - (responseTimes.length === 1 ? 0 : cumulativeCost - responseTimes[responseTimes.length - 2])
-  //   : 0;
-      
   return (
     <div className="container">
       <h1>Database Response Time Over Time</h1>
@@ -205,7 +315,6 @@ function App() {
         <Legend />
         <Line type="monotone" dataKey="cumulativeCost" name="Cumulative Cost" stroke="#82ca9d" />
       </LineChart>
-
     </div>
   );
 }
