021902800b05391a5d88a16e2643a85abeaa95c6
dtairych-equinix <dtairych@ap.equinix.com>
2023-10-23 16:45:29 +0200

    Updated app to find delta in cost changes

--- src/App.js
+++ src/App.js
@@ -21,65 +21,75 @@ function App() {
   const [chartData, setChartData] = useState([]);
   const [costForLastRequestDisplay, setCostForLastRequestDisplay] = useState(0);
   const [currentInterval, setCurrentInterval] = useState(1);
+  const [costFactorChanges, setCostFactorChanges] = useState([]);
 
   const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:4000';  // default to localhost if not provided
 
   useEffect(() => {
     const pollServer = async () => {
       try {
+        // ... existing code ...
+
         const costFactorResponse = await axios.get(`${apiUrl}/get-cost-factor`);
         const { selectedCostKey, selectedCostValue } = costFactorResponse.data;
         setSelectedCostKey(selectedCostKey);
         setSelectedCostValue(selectedCostValue);
-        console.log("Selected Cost Value: ", selectedCostValue)
-  
+
+        if (costFactorChanges.length === 0 || selectedCostValue !== costFactorChanges[costFactorChanges.length - 1].costFactor) {
+          setCostFactorChanges(prev => [...prev, { interval: currentInterval, costFactor: selectedCostValue }]);
+        }
+        
         const response = await axios.get(`${apiUrl}/poll`);
-        // setCurrentInterval(prevInterval => prevInterval + 1);
         const data = response.data;
-  
+
         setTotalRecords(data.totalRecords);
-  
         setResponseTimes((prevResponseTimes) => [...prevResponseTimes, data.responseTime].slice(-300));
-  
+
         const dataSizeInMB = data.totalRecords * calculateDataSizePerRecordInMB();
         setCumulativeDataSize((prevCumulativeDataSize) => prevCumulativeDataSize + dataSizeInMB);
-        
-  
+
         const costForLastRequest = dataSizeInMB * selectedCostValue;
         setCumulativeCost(prevCumulativeCost => prevCumulativeCost + costForLastRequest);
 
-        console.log("Data Size in MB:", dataSizeInMB);
-        console.log("Cost for Last Request:", costForLastRequest);
-        console.log("Previous Cumulative Cost:", cumulativeCost);
-
-
         setChartData((prevChartData) => {
-          
-          
           const newEntry = {
-            // Using currentInterval + 1 because the state update is scheduled, but not yet effective.
             interval: currentInterval + 1,
             responseTime: data.responseTime,
             cumulativeCost: prevChartData.length === 0 
                 ? costForLastRequest 
-                : (prevChartData[prevChartData.length - 1].cumulativeCost + costForLastRequest)
-          };  
-          setCurrentInterval(prevInterval => prevInterval + 1);
+                : (prevChartData[prevChartData.length - 1].cumulativeCost + costForLastRequest),
+            projectedCosts: costFactorChanges.map((change, index) => {
+              if (newEntry.interval < change.interval) {
+                return 0;
+              } else {
+                const previousInterval = change.interval === 0 ? 0 : change.interval - 1;
+                const previousData = prevChartData.find(d => d.interval === previousInterval) || { projectedCosts: Array(costFactorChanges.length).fill(0) };
+                return previousData.projectedCosts[index] + (dataSizeInMB * change.costFactor);
+              }
+            })
+          };
+
           return [...prevChartData, newEntry].slice(-300);
         });
 
+        setCurrentInterval(prevInterval => prevInterval + 1);
         setCostForLastRequestDisplay(costForLastRequest);
       } catch (error) {
         console.log('Error fetching data -', error);
       }
     };
-  
+
     const interval = setInterval(pollServer, 10000);
     return () => clearInterval(interval);
-  }, []); 
+  }, [selectedCostValue, currentInterval]);
 
   const calculateDataSizePerRecordInMB = () => {
     return 0.1; // Assuming each record is 0.1 MB for illustration purposes
+  };
+
+  const getLineColor = (index) => {
+    const colors = ['#8884d8', '#82ca9d', '#ffc658', '#ff8042', '#8884d8']; // Add more colors as needed
+    return colors[index % colors.length];
   };
 
   return (
@@ -107,6 +117,16 @@ function App() {
         <Tooltip />
         <Legend />
         <Line type="monotone" dataKey="cumulativeCost" name="Cumulative Cost" stroke="#82ca9d" />
+        {costFactorChanges.map((change, index) => (
+          <Line 
+            key={index}
+            type="monotone" 
+            dataKey={`projectedCosts[${index}]`} 
+            name={`Projected Cost (Phase ${index + 1})`} 
+            stroke={getLineColor(index)} 
+            dot={false}
+          />
+        ))}
       </LineChart>
     </div>
   );
